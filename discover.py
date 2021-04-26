88cd650df5b817320c90495cc33082ac04ba4b7a
Duncedrin <evdelmolino@gmail.com>
2021-04-26 19:50:10 -0400

    Create discover.py

---
+++ discover.py
@@ -0,0 +1,83 @@
+import os
+from tkinter import filedialog
+
+#steamLoc = ["C:\Program Files (x86)\Steam\steamapps\common","D:\Windows\Steam\steamapps\common"]
+steamLoc = []
+steamGames = []
+#battleNetLoc = ["D:\Windows\BattleNet"]
+battleNetLoc = []
+battleNet = []
+originLoc = []
+origin = []
+uplayLoc = []
+uplay = []
+
+
+def steamAdd():
+    steamLoc.append(filedialog.askdirectory())
+
+def battleAdd():
+    battleNetLoc.append(filedialog.askdirectory())
+
+def originAdd():
+    originLoc.append(filedialog.askdirectory())
+
+def uplayAdd():
+    uplayLoc.append(filedialog.askdirectory())
+
+def location():
+    ans = "blah"
+    while ans != "finish":
+        print("Game Library Input, please select an option:")
+        print("Steam")
+        print("Origin")
+        print("BattleNet")
+        print("Uplay")
+        print("JSON")
+        print("finish")
+        ans = input("")
+        if ans == "Steam":
+            steamAdd()
+        if ans == "BattleNet":
+            battleAdd()
+        if ans == "Origin":
+            originAdd()
+        if ans == "Uplay":
+            uplayAdd()
+
+def find():
+    for i in steamLoc:
+        found = os.listdir(i)
+        for j in found:
+            steamGames.append(j)
+    for i in battleNetLoc:
+        found = os.listdir(i)
+        for j in found:
+            battleNet.append(j)
+    for i in originLoc:
+        found = os.listdir(i)
+        for j in found:
+            origin.append(j)
+    for i in uplayLoc:
+        found = os.listdir(i)
+        for j in found:
+            uplay.append(j)
+
+    print("Steam Games Found across all Drives:\n")
+    for i in steamGames:
+        print(i)
+    print("\nBattle.Net Games Found across all Drives:\n")
+    for i in battleNet:
+        print(i)
+    print("\nUplay Games Found across all Drives:\n")
+    for i in uplay:
+        print(i)
+    print("\nOrigin Games Found across all Drives:\n")
+    for i in origin:
+        print(i)
+
+def main():
+    location()
+    find()
+
+main()
